2d7c0e870af86e3043a0541ff9051bd49b3cd08f
Jiexin Guo <jiexing.jg@gmail.com>
2020-01-26 21:58:53 -0800

    Commit the whole android studio project.

---
+++ android/app/src/main/kotlin/com/guojiex/double_entry_bookkeeping/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.guojiex.double_entry_bookkeeping
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}

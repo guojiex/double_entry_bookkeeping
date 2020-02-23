5fa5a8e5811f377cee36eddaa1976fa3af28d396
guojiex <1036479561@qq.com>
2020-02-23 11:29:46 -0800

    Import collection for priority queue.

--- lib/model/ledger.dart
+++ lib/model/ledger.dart
@@ -1,6 +1,9 @@
 // 账本
 import 'package:double_entry_bookkeeping/model/account.dart';
+import 'package:double_entry_bookkeeping/model/transaction.dart';
+import 'package:collection/collection.dart';
 
 class Ledger {
-  Map<String, Account> accountMap;
+  List<Account> accounts;
+  PriorityQueue<Transaction> transactions;
 }
